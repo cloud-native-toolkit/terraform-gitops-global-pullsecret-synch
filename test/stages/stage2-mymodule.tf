ebdf77553eef1b50c616772f67357e6768d004b8
Andrew Trice <amtrice@us.ibm.com>
2022-11-11 16:46:12 -0500

    updated namespace dependencies
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -4,6 +4,6 @@ module "global_pull_secret_synch" {
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
-  namespace = module.gitops_namespace.name
+  namespace = module.dev_tools_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
 }
